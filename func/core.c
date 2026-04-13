4bf3caf2f1ba37bb48228a057929fef6e5c7e246
GoogolFisch <florian.losensky@yahoo.com>
2026-04-13 20:44:35 +0200

    moved expr into seperate file

--- func/core.c
+++ func/core.c
@@ -1,5 +1,6 @@
 
 #include"load.h"
+#include"expr.h"
 
 TCL_String *putsFunction(TCLR_Context **ctx, TCLS_Cmd *cmd){
 	// puts {value} ;# for now
@@ -51,58 +52,6 @@ TCL_String *procFunction(TCLR_Context **ctx,TCLS_Cmd *cmd){
 	return NULL;
 }
 
-
-typedef struct TCLCORE_LIST_Expr{
-	int32_t flags;
-	TCL_Slice *str;
-	struct TCLCORE_KV_Expr *left;
-	struct TCLCORE_KV_Expr *right;
-} TCLCORE_LIST_Expr;
-typedef struct TCLCORE_Expr{
-	int32_t capacity;
-	int32_t length;
-	TCLCORE_LIST_Expr expr[];
-} TCLCORE_Expr;
-TCL_String *exprFunctionFree(TCLR_Context **ctx,TCLS_Cmd *cmd){
-	ctx = ctx;
-	// proc {name} {args} {body}
-	printf("free %.*s\n",cmd->command->length,cmd->command->data);
-	for(int32_t i = 0;i < cmd->length;i++){
-		printf("- %.*s\n",cmd->arguments[i]->length,cmd->arguments[i]->data);
-	}
-	if(cmd->length != 1){
-		printf("Err with set (d0378443-5e77-4361-886d-45d58a2691da)\n");
-		return NULL;
-	}
-	/*
-	TCLCORE_Expr *exprPtr = (TCLCORE_Expr*)(cmd->moreData);
-	for(int32_t ovExpr = 0;ovExpr < exprPtr->length;ovExpr++){
-		free(exprPtr->);
-	}
-	*/
-	if(cmd->moreData != NULL){
-		// TODO also free potential memory in the LIST
-		free(cmd->moreData);
-	}
-	return NULL;
-}
-TCL_String *exprFunction(TCLR_Context **ctx,TCLS_Cmd *cmd){
-	ctx = ctx;
-	// proc {name} {args} {body}
-	printf("fn %.*s\n",cmd->command->length,cmd->command->data);
-	for(int32_t i = 0;i < cmd->length;i++){
-		printf("- %.*s\n",cmd->arguments[i]->length,cmd->arguments[i]->data);
-	}
-	if(cmd->length != 1){
-		printf("Err with set (62bf270b-ba0d-44ca-8bd2-5498044247ed)\n");
-		return NULL;
-	}
-	TCLCORE_Expr *exprPtr = malloc(sizeof(TCLCORE_Expr) +
-			sizeof(TCLCORE_LIST_Expr) * 128);
-	exprPtr->capacity = 128;
-	exprPtr->length = 0;
-	return NULL;
-}
 
 #ifdef FN_REDEF_INIT
 #define load_init fn_load_core_init
---
+++ func/expr.h
@@ -0,0 +1,105 @@
+
+#ifndef FUNC_CORE_EXPR_H_
+#define FUNC_CORE_EXPR_H_
+
+typedef struct TCLCORE_LIST_Expr{
+	int32_t flags;
+	TCL_Slice str;
+	struct TCLCORE_KV_Expr *left;
+	struct TCLCORE_KV_Expr *right;
+} TCLCORE_LIST_Expr;
+typedef struct TCLCORE_Expr{
+	int32_t capacity;
+	int32_t length;
+	TCLCORE_LIST_Expr expr[];
+} TCLCORE_Expr;
+
+void exprTokenise(TCLCORE_Expr **exprList,TCL_String *str){
+	int32_t idx, exprOff;
+	exprOff = 0;
+	TCLCORE_Expr *expr = *exprList;
+	if(expr->capacity == 0){
+		expr->capacity = 8;
+		(*exprList) = realloc(expr,sizeof(TCLCORE_Expr) +
+				sizeof(TCLCORE_LIST_Expr) * expr->capacity);
+		expr = *exprList;
+	}
+	expr->expr[0] = (TCLCORE_LIST_Expr){0};
+	expr->expr[0].str = (TCL_Slice){   .refs = 1, .tags = 0,
+		.length = 0, .offset = 0,  .string = str,
+	};
+	//
+	char state = 0;
+	idx = 0;
+	goto expr_token_skip_first;
+	for(;idx < str->length;idx++){
+		if(expr->length >= expr->capacity){
+			expr->capacity *= 2;
+			size_t sz = sizeof(TCLCORE_Expr);
+			sz += sizeof(TCLCORE_LIST_Expr) * expr->capacity;
+			(*exprList) = realloc(expr,sz);
+			expr = *exprList;
+		}
+		expr->expr[exprOff] = (TCLCORE_LIST_Expr){0};
+		expr->expr[exprOff].str = (TCL_Slice){ .refs = 1, .tags = 0,
+			.length = 0, .offset = idx,  .string = str,
+		};
+		//
+expr_token_skip_first:
+		expr->expr[exprOff].str.length++;
+		if(state == 0){
+			state = str->data[idx];
+			if(state >= 'A' && state <= 'Z') state = ':';
+			if(state >= 'a' && state <= 'z') state = ':';
+			if(state == '_') state = ':';
+			if(state >= '0' && state <= '9') state = '0';
+			//if(state == '.') state = '.'
+		}
+	}
+}
+
+
+
+
+TCL_String *exprFunctionFree(TCLR_Context **ctx,TCLS_Cmd *cmd){
+	ctx = ctx;
+	// proc {name} {args} {body}
+	printf("free %.*s\n",cmd->command->length,cmd->command->data);
+	for(int32_t i = 0;i < cmd->length;i++){
+		printf("- %.*s\n",cmd->arguments[i]->length,cmd->arguments[i]->data);
+	}
+	if(cmd->length != 1){
+		printf("Err with set (d0378443-5e77-4361-886d-45d58a2691da)\n");
+		return NULL;
+	}
+	/*
+	TCLCORE_Expr *exprPtr = (TCLCORE_Expr*)(cmd->moreData);
+	for(int32_t ovExpr = 0;ovExpr < exprPtr->length;ovExpr++){
+		free(exprPtr->);
+	}
+	*/
+	if(cmd->moreData != NULL){
+		// TODO also free potential memory in the LIST
+		free(cmd->moreData);
+	}
+	return NULL;
+}
+TCL_String *exprFunction(TCLR_Context **ctx,TCLS_Cmd *cmd){
+	ctx = ctx;
+	// proc {name} {args} {body}
+	printf("fn %.*s\n",cmd->command->length,cmd->command->data);
+	for(int32_t i = 0;i < cmd->length;i++){
+		printf("- %.*s\n",cmd->arguments[i]->length,cmd->arguments[i]->data);
+	}
+	if(cmd->length != 1){
+		printf("Err with set (62bf270b-ba0d-44ca-8bd2-5498044247ed)\n");
+		return NULL;
+	}
+	TCLCORE_Expr *exprPtr = malloc(sizeof(TCLCORE_Expr) +
+			sizeof(TCLCORE_LIST_Expr) * 128);
+	exprPtr->capacity = 128;
+	exprPtr->length = 0;
+	return NULL;
+}
+
+#endif
