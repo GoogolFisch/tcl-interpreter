dc97c144095e34024f401055cff14edf04fe2dc6
GoogolFisch <florian.losensky@yahoo.com>
2026-04-14 10:53:01 +0200

    op-parsing

--- func/expr.h
+++ func/expr.h
@@ -2,11 +2,19 @@
 #ifndef FUNC_CORE_EXPR_H_
 #define FUNC_CORE_EXPR_H_
 
+enum EXPR_LIST_Flags{
+	EXPR_LISTF_FREE = 0,
+	EXPR_LISTF_USED = 1,
+};
 typedef struct TCLCORE_LIST_Expr{
-	int32_t flags;
+	enum EXPR_LIST_Flags flags;
 	TCL_Slice str;
+	int32_t left;
+	int32_t right;
+	/* /
 	struct TCLCORE_KV_Expr *left;
 	struct TCLCORE_KV_Expr *right;
+	//  */
 } TCLCORE_LIST_Expr;
 typedef struct TCLCORE_Expr{
 	int32_t capacity;
@@ -32,21 +40,21 @@ void exprTokenise(TCLCORE_Expr **exprList,TCL_String *str){
 	char state = 0;
 	char move;
 	idx = 0;
-	goto expr_token_skip;
+	goto expr_token_append;
 	for(;idx < str->length;idx++){
 		move = str->data[idx];
 		if((uint8_t)      move >= 128) move = ':';
 		if(move >= 'A' && move <= 'Z') move = ':';
 		if(move >= 'a' && move <= 'z') move = ':';
-		if(move == '_') move = ':';
+		if(               move == '_') move = ':';
 		if(move >= '0' && move <= '9') move = '0';
 		//
-		if(state == '$' && move == ':')goto expr_token_skip;
-		if(state == '0' && move == '0')goto expr_token_skip;
-		if(state == ':' && move == ':')goto expr_token_skip;
+		if(state == '$' && move == ':') goto expr_token_append;
+		if(state == '0' && move == '0') goto expr_token_append;
+		if(state == ':' && move == ':') goto expr_token_append;
 		if(state == '0' && move == '.'){
 			state = '.';
-			goto expr_token_skip;
+			goto expr_token_append;
 		}
 		if(move == ' ' || move == '\t' ||
 				move == '\r' || move == '\n'){
@@ -66,18 +74,55 @@ void exprTokenise(TCLCORE_Expr **exprList,TCL_String *str){
 			.length = 0, .offset = idx,  .string = str,
 		};
 		//
-expr_token_skip:
+expr_token_append:
 		expr->expr[exprOff].str.length++;
 		if(state == 0){
 			state = str->data[idx];
 			if((uint8_t)       state >= 128) state = ':';
 			if(state >= 'A' && state <= 'Z') state = ':';
 			if(state >= 'a' && state <= 'z') state = ':';
-			if(state == '_') state = ':';
+			if(                state == '_') state = ':';
 			if(state >= '0' && state <= '9') state = '0';
 			//if(state == '.') state = '.';
 		}
 	}
+}
+void exprTokensFree(TCLCORE_Expr *exprList){
+	/*
+	for(int32_t idx = 0:idx < expr->length;idx++){
+	}
+	//   */
+	free(exprList);
+}
+
+int32_t exprTokenOperationTree(TCLCORE_Expr *exprList,int32_t home,
+		int32_t lower,int32_t upper,
+		char inFlags){
+
+	int32_t left  = home - 1;
+	int32_t right = home + 1;
+	if(inFlags == 0)inFlags |= 3;
+	if(inFlags & 1){
+		while(left  >= lower){
+			if(exprList->expr[left ].flags == EXPR_LISTF_FREE)
+				break;
+			left --;
+		}
+		if(left  >= lower)return 1;
+		exprList->expr[left ].flags = EXPR_LISTF_USED;
+		exprList->expr[home ].left  = left;
+	}
+	if(inFlags & 2){
+		while(right < upper){
+			if(exprList->expr[right].flags == EXPR_LISTF_FREE)
+				break;
+			right++;
+		}
+		if(right < upper)return 1;
+		exprList->expr[right].flags = EXPR_LISTF_USED;
+		exprList->expr[home ].right = right;
+	}
+	return 0;
 }
 
 
